e298047f03cdba3fdf7fa902aa71f2b1167b7f87
jungbee2 <126428632+jungbee2@users.noreply.github.com>
2025-01-06 18:20:19 +0900

    ✨feat: sectionSelectPage

--- src/Pages/SectionSelectPage.tsx
+++ src/Pages/SectionSelectPage.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Link } from 'react-router-dom'; 
 import 메인 from '../assets/images/ex.png';
 
@@ -15,20 +14,14 @@ const styles: { [key: string]: React.CSSProperties } = {
     fontWeight:'bold',
   },
 };
-=======
-import { useNavigate } from "react-router-dom";
->>>>>>> 213aea6d4dc9c612a172eca13e95c55c74dbae0e
 
 const SectionSelectPage = () => {
-  const navigate = useNavigate();
-
   return (
     <div>
       <div style={{ position: 'relative' }}>
         <img src={메인} alt="메인" style={{ width: '100%' }} />
       </div>
       <div>
-<<<<<<< HEAD
       <Link to="/signup" style={{ textDecoration: 'none' }}>
           <button
             style={{ ...styles.button, position: 'absolute', top: '70%', left: '30%', transform: 'translate(-50%, -50%)' }}
@@ -50,11 +43,6 @@ const SectionSelectPage = () => {
             디자인 파트
           </button>
         </Link>
-=======
-        <button onClick={() => navigate("/backend-question")}>백엔드 파트</button>
-        <button onClick={() => navigate("/frontend-question")}>프론트엔드 파트</button>
-        <button onClick={() => navigate("/design-question")}>디자인 파트 </button>
->>>>>>> 213aea6d4dc9c612a172eca13e95c55c74dbae0e
       </div>
     </div>
   )
