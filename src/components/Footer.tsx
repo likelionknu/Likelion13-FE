024cbabc457dbe75be1eea00e65b41038ce03cd2
jungbee2 <126428632+jungbee2@users.noreply.github.com>
2025-01-27 20:49:29 +0900

    🎨 style/Main_Intro_Footer_Img+✨Img_Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,45 +1,33 @@
-// import { FaInstagram } from 'react-icons/fa'
-// 인스타그램 아이콘 추가
-
-const styles = {
-  footer: {
-    backgroundColor: '#F8F8F8',
-    color: '#B3B3B3',
-    marginTop: '50px',
-    padding: '20px 0',
-  },
-  linkContainer: {
-    display: 'flex',
-    justifyContent: 'center',
-    margin: '60px 0',
-  },
-  link: {
-    color: '#B3B3B3',
-    textDecoration: 'none',
-    margin: '0 15px',
-    fontSize: '18px',
-    display: 'flex',
-    alignItems: 'center',
-  },
-
-};
+import '../assets/Footer.css'
+import 인스타 from '../assets/images/ft_insta.png';
+import 멋사2 from '../assets/images/ft_knu.png';
+import 멋사3 from '../assets/images/ft_lion3.png';
+import 멋사4 from '../assets/images/ft_lion4.png';
 
 const Footer = () => {
   return (
-    <footer style={styles.footer}>
-      <div style={styles.linkContainer}>
-        <a href='https://www.instagram.com/likelion.knu/' style={styles.link}>인스타그램</a>
-        <a href='https://www.naver.com/'  style={styles.link}>멋쟁이사자</a>
-        <a href='https://www.naver.com/'  style={styles.link}>임시링크1</a>
-        <a href='https://www.naver.com/'  style={styles.link}>임시링크2</a>
+    <footer className="footer">
+      <div className="link-container">
+        <a href="https://www.instagram.com/likelion.knu/" className="link">
+          <img src={인스타} alt="인스타그램" />
+        </a>
+        <a href="https://www.naver.com/" className="link">
+          <img src={멋사2} alt="멋사2" />
+        </a>
+        <a href="https://www.naver.com/" className="link">
+          <img src={멋사3} alt="멋사3" />
+        </a>
+        <a href="https://www.naver.com/" className="link">
+          <img src={멋사4} alt="멋사4" />
+        </a>
       </div>
 
-      <div>
-        <p>©2024 LIKE LION KNU. All rights reserved.</p>
-        <p>동아리실: 경기도 용인시 기흥구 강남로 40 강남대학교 후생관</p>
+      <div className="bt-container">
+        <p className="bt-font">©2024 LIKE LION KNU. All rights reserved.</p>
+        <p className="bt-font">동아리실: 경기도 용인시 기흥구 강남로 40 강남대학교 후생관</p>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
