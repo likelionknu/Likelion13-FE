b08410404367e5d7424ad46542c60dd77340c99c
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2025-02-06 21:17:12 +0900

    🎨 style : Page-Animation, Navbar-Dropdown
    - Nav바 지원하기 > 드롭다운 메뉴 추가
    - gsap 라이브러리 추가
    - 페이지 전환 애니메이션 추가

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
+
+import gsap from 'gsap'
+
 import Nav from '../components/Nav'
 import MainPage from '../Pages/MainPage'
 import MyPage from '../Pages/MyPage'
@@ -17,77 +20,115 @@ import ProjectIntroPage from '../Pages/ProjectIntroPage'
 
 import './Routes.css'
 
-const ScrollToTop = () => {
-  const location = useLocation();
-  
-  useEffect(() => {
-    window.scrollTo({top: 0, behavior: "smooth" });
-  }, [location.pathname]);
+import { ReactNode } from 'react'
 
-  return null;
-};
+const PageTransition = ({ children }: { children: ReactNode }) => {
+  const element = useRef(null)
+  const location = useLocation()
+
+  useEffect(() => {
+    // 진입 애니메이션
+    gsap.fromTo(
+      element.current,
+      {
+        opacity: 0,
+        y: 1,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1, // 1.5초 동안
+        ease: 'power1.out',
+      }
+    )
+  }, [location])
+
+  return <div ref={element}>{children}</div>
+}
+
+const ScrollToTop = () => {
+  const location = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }, [location.pathname])
+
+  return null
+}
 
 const Router = () => {
   return (
     <BrowserRouter>
-    <ScrollToTop />
-    <div className = "pages-container">
-      <Nav />
-      <Routes>
-        <Route
-          path='/'
-          element={<MainPage />}
-        />
-        <Route
-          path='/*'
-          element={<MainPage />}
-        />
-        <Route
-          path='/project-introduce'
-          element={<ProjectIntroPage />}
+      <ScrollToTop />
+      <div className='pages-container'>
+        <Nav />
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <PageTransition>
+                <MainPage />
+              </PageTransition>
+            }
           />
-        <Route
-          path='/mypage'
-          element={<MyPage />}
-        />
-        <Route
-          path='/login'
-          element={<LoginPage />}
-        />
-        <Route
-          path='/signup'
-          element={<SignupPage />}
-        />
-        <Route
-          path='/SectionSelect'
-          element={<SectionSelectPage />}
-        />
-        <Route
-          path='/backend-question'
-          element={<BackendQuestionPage />}
-        />
-        <Route
-          path='/frontend-question'
-          element={<FrontendQuestionPage />}
-        />
-        <Route
-          path='/design-question'
-          element={<DesignQuestionPage />}
-        />
-        <Route
-          path='/FrontIntro'
-          element={<FrontIntroPage/>}
-        />
-        <Route
-          path='/BackIntro'
-          element={<BackIntroPage/>}
-        />
-        <Route
-          path='/DesignIntro'
-          element={<DesignIntroPage/>}
-        />
-      </Routes>
-      <Footer />
+          <Route
+            path='/*'
+            element={<MainPage />}
+          />
+          <Route
+            path='/project-introduce'
+            element={
+              <PageTransition>
+                <ProjectIntroPage />
+              </PageTransition>
+            }
+          />
+          <Route
+            path='/mypage'
+            element={<MyPage />}
+          />
+          <Route
+            path='/login'
+            element={
+              <PageTransition>
+                <LoginPage />
+              </PageTransition>
+            }
+          />
+          <Route
+            path='/signup'
+            element={<SignupPage />}
+          />
+          <Route
+            path='/SectionSelect'
+            element={<SectionSelectPage />}
+          />
+          <Route
+            path='/backend-question'
+            element={<BackendQuestionPage />}
+          />
+          <Route
+            path='/frontend-question'
+            element={<FrontendQuestionPage />}
+          />
+          <Route
+            path='/design-question'
+            element={<DesignQuestionPage />}
+          />
+          <Route
+            path='/FrontIntro'
+            element={<FrontIntroPage />}
+          />
+          <Route
+            path='/BackIntro'
+            element={<BackIntroPage />}
+          />
+          <Route
+            path='/DesignIntro'
+            element={<DesignIntroPage />}
+          />
+        </Routes>
+        <Footer />
       </div>
     </BrowserRouter>
   )
