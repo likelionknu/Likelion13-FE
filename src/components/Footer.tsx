52fad6eb4d2a13d81d6165708ac28428f47c17a1
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2025-02-22 23:13:54 +0900

    🚑 fix :Fix footer.tsx
    - 푸터 멘트 조금 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,8 +23,8 @@ const Footer = () => {
           </a>
         </div>
         <div className="bt-container">
-          <p className="bt-font">©2024 LIKE LION KNU. All rights reserved.</p>
-          <p className="bt-font">동아리실: 경기도 용인시 기흥구 강남로 40 강남대학교 후생관</p>
+          <p className="bt-font">©2025 LIKELION KNU. All rights reserved.</p>
+          <p className="bt-font">경기도 용인시 기흥구 강남로 40 강남대학교 후생관</p>
         </div>
       </div>
     </footer>
