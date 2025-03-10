88cc088c4dffee4ac705bbf60f4aa5e046ad2bc5
o11117 <115409472+o11117@users.noreply.github.com>
2025-03-10 14:51:20 +0900

    🚑 Hotfix : block console

--- src/App.tsx
+++ src/App.tsx
@@ -28,8 +28,10 @@ function App() {
               token: token,
             })
           }
+        /*
         })
         .catch((error) => {
+        */
           // console.error('로그인 상태 복원 실패:', error)
           localStorage.removeItem('token') // 토큰이 유효하지 않으면 제거
         })
